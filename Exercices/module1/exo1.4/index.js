3b82748e91a840115c75d8b51747455749367194
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2022-09-26 07:00:55 +0100

    modern-hmi

--- Exercices/module1/exo1.4/index.js
+++ Exercices/module1/exo1.4/index.js
@@ -1,11 +1,40 @@
 
-const  divs = document.querySelector(".div");
+let divred = document.querySelector('.red');
+let divorange= document.querySelector(".orange");
+let divgreen= document.querySelector("green");
 
-const redColor="red";
-const orangeColor="orange";
-const greenColor="green";
+const colorSequence = ['red', 'orange', 'green', 'orange'];
+let i = 0; 
+const emptyColor="black";
 
-// A refaire pour comprendre.!!
+const delayBetweenChange=1000;
+/*setInterval(changeColorWithDelay,delayBetweenChange);
+
+/*function changeColorWithDelay (){
+        if(i==colorSequence.length)
+            i=0;
+		
+        if(colorSequence[i]==="red"){
+            divred.style.backgrounColor="red";
+            divgreen.style.backgrounColor=emptyColor;
+            divorange.style.backgrounColor=emptyColor;
+        }else
+        if(colorSequence[i]==="orange"){
+            divorange.style.backgrounColor="orange";
+            divred.style.backgrounColor=emptyColor;
+            divgreen.style.backgrounColor=emptyColor;
+		}  else
+		if(colorSequence[i]==="green"){
+            divgreen.style.backgrounColor="red";
+            divgreen.style.backgrounColor=emptyColor;
+            divorange.style.backgrounColor=emptyColor;
+        } 
+        i++;
+     
+    }
+*/
+
+
 
 
 
