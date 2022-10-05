dd91068e88fa2913e213848c8402851f4d768c0f
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2022-10-05 11:53:28 +0100

    exercices2.3

--- Exercices/module2/exo2.3/src/index.js
+++ Exercices/module2/exo2.3/src/index.js
@@ -13,29 +13,36 @@ input.addEventListener('click', createHtmlTableOfStrings);
 function createArray (lignes,colonnes,string){
     let table= []
 
-   for(let i=0; i< nbrLignes; i++){
+   for(let i=0; i< lignes+1; i++){
     table.push([]);
-    for(let j; j<nbrcolonnes; j++){
+    for(let j; j<colonnes+1; j++){
        table[i].push(string + "["+ i+"]"+"["+j+"]");  
     }
    }
  return table;
 }
 
-  function createHtmlTableOfStrings(createArray(nbrLignes, nbrcolonnes,initString)){
-    ligne = createArray.ligne;
-    colonne = createArray.colonne;
-    string= createArray.string;
-    const ligneHtml= document.querySelector('tr');
-    for (let i = 0; i < createArray.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-           ligneHtml.innerHTML=createArray[i][j];
-
-        }
-        
-    }
-   
+  function createHtmlTableOfStrings( createArray(nbrLignes, nbrcolonnes,initString)){
+    // création des elts delimiteurs
+    const div = document.createElement('div');
     
+    const table = document.createElement('table');
+    table.innerHTML=createArray(nbrLignes,nbrcolonnes,initString);
+
+    div.className='table-responsive pt-5';
+    div.appendChild(table);
+
+
+    for(let i=0 ; i<createArray.length; i++ ){
+      const tr= document.createElement('tr');
+      for (let j = 0; j < createArray[0].length; j++){
+        const td= document.createElement('td');
+        td.innerHTML= createArray[i][j];
+         tr.appendChild(td);
+        
+      }
+    }
+    return table;
   } 
   
 
