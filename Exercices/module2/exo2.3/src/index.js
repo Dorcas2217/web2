30a8bbd8583e7780b33211b559ea93883ab0c364
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2022-10-13 09:28:35 +0100

    app express vian generateur

--- Exercices/module2/exo2.3/src/index.js
+++ Exercices/module2/exo2.3/src/index.js
@@ -2,47 +2,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 import 'animate.css';
 
-const nbrLignes = document.querySelector('#lignes');
-const nbrcolonnes = document.querySelector('#colonnes');
-const initString = document.querySelector('#string');
-const input = document.querySelector('value');
+const nbrLignes = document.querySelector('#lignes').value;
+const nbrcolonnes = document.querySelector('#colonnes').value;
+const initString = document.querySelector('#string').value;
+// const input = document.querySelector('value');
+const form = document.querySelector('form');
 
-
-input.addEventListener('click', createHtmlTableOfStrings);
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+ 
+  const main = document.querySelector('main');
+  main.appendChild(createHtmlTableOfStrings());
+});
 
 function createArray (lignes,colonnes,string){
-    let table= []
-
-   for(let i=0; i< lignes+1; i++){
+    const table= [];
+   for(let i=0; i< lignes; i+=1){
     table.push([]);
-    for(let j; j<colonnes+1; j++){
-       table[i].push(string + "["+ i+"]"+"["+j+"]");  
+    for(let j=0; j<colonnes; j+=1){
+       table[i].push(`${string}[${i}][${j}]`);  
     }
    }
  return table;
 }
 
-  function createHtmlTableOfStrings( createArray(nbrLignes, nbrcolonnes,initString)){
+function createHtmlTableOfStrings(){
     // création des elts delimiteurs
+    // demande au prof comment supprimer un repositori
+    
+     
     const div = document.createElement('div');
     
     const table = document.createElement('table');
-    table.innerHTML=createArray(nbrLignes,nbrcolonnes,initString);
+    const jsTable = createArray(nbrLignes,nbrcolonnes,initString);
 
     div.className='table-responsive pt-5';
-    div.appendChild(table);
-
-
-    for(let i=0 ; i<createArray.length; i++ ){
+    
+    for(let i=0 ; i<jsTable.length; i += 1 ){
       const tr= document.createElement('tr');
-      for (let j = 0; j < createArray[0].length; j++){
+      for (let j = 0; j < jsTable[0].length; j+=1){
         const td= document.createElement('td');
-        td.innerHTML= createArray[i][j];
+        td.innerHTML= jsTable[i][j];
          tr.appendChild(td);
         
       }
+      table.appendChild(tr);
     }
-    return table;
+    
+    div.appendChild(table);
+    return div;
   } 
   
 
