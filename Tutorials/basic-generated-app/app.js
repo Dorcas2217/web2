6f692b00f478b133a343142ff475afc89615b3f7
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2022-11-14 12:28:01 +0100

    first commit

--- Tutorials/basic-generated-app/app.js
+++ Tutorials/basic-generated-app/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 module.exports = app;
